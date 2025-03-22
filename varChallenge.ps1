41fa4bc737e3b705d42a34ad5dfbc686b3ee5db9
nicole732 <dketchiozo@gmail>
2025-03-22 18:49:48 -0400

    variable time and files in PS

---
+++ varChallenge.ps1
@@ -0,0 +1,13 @@
+﻿#get information from use
+$firstName = Read-Host -Prompt "Please enter your  first name"
+$lastName = Read-Host -Prompt "Please enter your last  name"
+
+#get date
+$today = Get-Date -format "MMddyyyy-HHmmss"
+$longDate = Get-Date -Format "F"
+Write-Host "Today is $today"
+
+#write the info to a file
+Set-Content -Value "First Name: $firstName
+Last Name: $lastName
+Date: $longDate" -Path "./$today-$firstName-$lastName.txt"
